039b0abcb05061ae11a16337c5ce8317a68be5df
jeffworley <sirwhirls@gmail.com>
2015-09-27 19:56:08 -0400

    Met the target by programming all board states and moving through them by adding a selected class and removing the selected class.  Adjusted script links in html as well.

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,5 @@
+//Credit for this logic goes to jmcreaseman.  I had originally started down a path that would have changed the piece locations within the same board array and hope to still work independently to reach that goal.
+
 console.log("Loaded controllers.js");
 (function(globals){ // What is this even? Do I need it?
   /**
@@ -18,14 +20,28 @@ console.log("Loaded controllers.js");
     // Maybe do something with EVENT...?
   }
 
+  var cmCounter = 0;
+  var totalBoardStates = 9;
+
 
   // Controller for "next move"...
   jQuery("#step-forward").on('click', function(event){
     // TODO: Fire tracer bullet!
     console.log("clicked the step-forward button!!!")
     // TODO: Tell the Model -- `game` -- to advance to the next move...
-    game.next();
+    //game.next();
     // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
+    var curID = "#boardState-" + cmCounter;
+    var nextID = curID;
+
+    if (cmCounter < totalBoardStates) {
+      cmCounter = cmCounter + 1;
+      nextID = "#boardState-" + cmCounter;
+
+      $(curID).addClass("notCurMove");
+      $(nextID).removeClass("notCurMove");
+    }
+
   });
 
   // Controller for "previous move"...
@@ -33,8 +49,20 @@ console.log("Loaded controllers.js");
     // TODO: Fire tracer bullet!
     console.log("clicked the step-back button!!!")
     // TODO: Tell the Model -- `game` -- to advance to the previous move...
-    game.prev();
+    //game.prev();
     // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
+
+    var curID = "#boardState-" + cmCounter;
+    var prevID = curID;
+
+    if (cmCounter > 0) {
+      cmCounter = cmCounter - 1;
+      prevID = "#boardState-" + cmCounter;
+
+      $(curID).addClass("notCurMove");
+      $(prevID).removeClass("notCurMove");
+    }
+
   });
 
   // Controller for "fast-forward"...
@@ -42,8 +70,20 @@ console.log("Loaded controllers.js");
     // TODO: Fire tracer bullet!
     console.log("clicked the fast-forward button!!!")
     // TODO: Tell the Model -- `game` -- to advance to the last move...
-    game.end();
+    //game.end();
     // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
+
+    $("#boardState-0").addClass("notCurMove");
+    $("#boardState-1").addClass("notCurMove");
+    $("#boardState-2").addClass("notCurMove");
+    $("#boardState-3").addClass("notCurMove");
+    $("#boardState-4").addClass("notCurMove");
+    $("#boardState-5").addClass("notCurMove");
+    $("#boardState-6").addClass("notCurMove");
+    $("#boardState-7").addClass("notCurMove");
+    $("#boardState-8").addClass("notCurMove");
+    $("#boardState-9").removeClass("notCurMove");
+
   });
 
   // Controller for anything else...
@@ -51,8 +91,20 @@ console.log("Loaded controllers.js");
     // TODO: Fire tracer bullet!
     console.log("clicked the reset button!!!")
     // TODO: Tell the Model -- `game` -- to do something it knows how to do...
-    game.reset();
+    //game.reset();
     // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
+
+    $("#boardState-0").removeClass("notCurMove");
+    $("#boardState-1").addClass("notCurMove");
+    $("#boardState-2").addClass("notCurMove");
+    $("#boardState-3").addClass("notCurMove");
+    $("#boardState-4").addClass("notCurMove");
+    $("#boardState-5").addClass("notCurMove");
+    $("#boardState-6").addClass("notCurMove");
+    $("#boardState-7").addClass("notCurMove");
+    $("#boardState-8").addClass("notCurMove");
+    $("#boardState-9").addClass("notCurMove");
+
   });
 
 // Am I supposed to recognize this?
