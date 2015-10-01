fee253a5d9504a26979ec8b943fd9543e4289640
jeffworley <sirwhirls@gmail.com>
2015-10-01 01:41:54 -0400

    Erased code from first attempt, added some new correct code, created mocha chai tests to try and address what chessboard functions should do and fixed file paths in package.json

--- js/main.js
+++ js/main.js
@@ -19,8 +19,29 @@
    * @var {Array} of...?
    */
   var moves = [
+    {from: {rank: 6, file: 3},
+    to: {rank: 4, file: 3}},
+    {from: {rank: 0, file: 6},
+    to: {rank: 2, file: 5}},
+    {from: {rank: 6, file: 2},
+    to: {rank: 4, file: 2}},
+    {from: {rank: 1, file: 4},
+    to: {rank: 2, file: 4}},
+    {from: {rank: 6, file: 6},
+    to: {rank: 5, file: 6}},
+    {from: {rank: 1, file: 3},
+    to: {rank: 3, file: 3}},
+    {from: {rank: 7, file: 5},
+    to: {rank: 6, file: 6}},
+    {from: {rank: 0, file: 5},
+    to: {rank: 1, file: 4}},
+    {from: {rank: 7, file: 6},
+    to: {rank: 5, file: 5}}
     // TODO: Fill me in!
   ]; // END moves
+
+  // Created var to cap number of moves
+  var totalMoves = 9;
 
   // Created var to keep track of moves
 
--- js/tests.js
+++ js/tests.js
@@ -25,13 +25,23 @@
 
     }); // END it should be a big Array
 
+    it('should tell us what game.reset returns', function(){
+      chai.expect(game.reset()).to.be.an('array');
+    });
+
+    it('should have the applyMove function', function(){
+      chai.expect(applyMove).to.be.a('function');
+    });
+
+
+
     it('should be able to move pieces', function(){
       // board[0][0] = board[0][1];
       game.applyMove(
         // TODO: one day take {file: 'd', rank: 2 },
-        {rank: 6, file: 3}, // from
+        {from: {rank: 6, file: 3}, // from
         // TODO: one day take {file: 'd', rank: 4 }
-        {rank: 4, file: 3} // to
+        to: {rank: 4, file: 3}} // to
       );
 
       var board = game.board();
@@ -43,6 +53,23 @@
 
     it.skip('should be able to advance to the next move', function(){
       // TODO: Maybe I should write some tests for this?
+      chai.expect(game.applyMove(Move[1])).to.be.equal({from: {rank: 0, file: 6}, to: {rank: 2, file: 5}}); // To confirm that applyMove is correctly moving through the Move array of dictionaries.
+      chai.expect(game.next()).to.be.equal(Move[1]); // To confirm that when game.next is invoked it is equal to the next index within the Move array of dictionaries.
+      // TODO: Figure out how to replace the [1] sub-index in the above Move array with a variable that changes as you iterate through the array. Maybe ++?
+    });
+
+    it.skip('should be able to step back to the prev move', function(){
+      chai.expect(game.applyMove(Move[0])).to.be.equal({from: {to: 6, file: 3}, from: {rank: 4, file: 3}}); // To confirm that applyMove is correctly stepping back through the array of dictionaries.
+      // TODO: Logic seems right for stepping backwards but NEED to figure out how to do that with a sub-index variable.  Maybe --?
+    });
+
+    it.skip('should reset the board to original state',function(){
+      chai.expect(game.reset()).to.be.equal(initial()); // To compare the result of game.reset to the result of initial, which would prove it is working.
+    });
+
+    it.skip('should show result of final move when game.end is invoked', function(){
+      chai.expect(game.end()).to.be.equal(totalMoves.result);  //Really have no clue here.  Just taking a shot in the dark as it is currently 1:40am.  Will revisit when rested.
+      // TODO: Need to solve how to get the array representation when game.next is equal to the value of totalMoves and then assigned to game.end.
     });
 
 })(window || module && module.exports || this);
